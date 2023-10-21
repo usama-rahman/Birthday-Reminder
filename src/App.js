36dd9e1bd5e93fecba37fedfd0bfc0b05f2738ce
Usama Rahman <rahmanusama390@gmail.com>
2023-10-21 22:50:04 +0600

    Done 🍉

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-// import React, { useState } from 'react';
-// import data from './data';
+import { useState } from 'react';
+import data from './data';
 import List from './List';
 function App() {
-  return <>
-    <main>
-      <section className='container'>
-        <h3>0 birthday today</h3>
-        <List />
-        <button onClick={() => console.log('You clicked me !')}>
-          clear all
-        </button>
-      </section>
-    </main>
-  </>;
+  const [people,setPeople] = useState(data)
+  return (
+    <>
+      <main>
+        <section className='container'>
+          <h3>{people.length} birthday today</h3>
+          <List people={people} />
+          <button onClick={() => setPeople([])}>
+            clear all
+          </button>
+        </section>
+      </main>
+    </>
+  )
 }
 
 export default App;
