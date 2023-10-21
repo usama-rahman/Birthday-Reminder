ecf0e2732e07962e26d037adab504c0cdda84242
Usama Rahman <rahmanusama390@gmail.com>
2023-10-21 22:30:24 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+https://uidesigndaily.com/
---
+++ src/App.js
@@ -0,0 +1,18 @@
+// import React, { useState } from 'react';
+// import data from './data';
+import List from './List';
+function App() {
+  return <>
+    <main>
+      <section className='container'>
+        <h3>0 birthday today</h3>
+        <List />
+        <button onClick={() => console.log('You clicked me !')}>
+          clear all
+        </button>
+      </section>
+    </main>
+  </>;
+}
+
+export default App;
